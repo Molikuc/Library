8df47f79ae11fc1919af0ba5c05e8d09ce0a1902
Molikuc <ruiz.alexis1996@gmail.com>
2021-12-08 12:48:01 +0100

    Add new script to add new book and display it. The new book push in the array and you can remove the book from the array and from the page. Didn't make the read/not read option yet. not the final result wish because it remove all the book.

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let myLibrary = [];
 let arrayLength = myLibrary.length;
 const addBook = document.querySelector('button');
 const form = document.querySelector('form');
+const title = document.getElementById('ftitle');
+const author = document.getElementById('fauthor');
+const pages = document.getElementById('fpages');
+const newBook = document.getElementById('library');
+const read = document.getElementById('fread');
+
 
 addBook.addEventListener('click', () =>{
     displayForm();
@@ -28,6 +34,46 @@ const dune = new Book('Dune', 'Frank Herbert', '412', 'not');
 
 function addBookToLibrary(){
 
+        const test = new Book(title.value, author.value, pages.value, );
+
+        const div = document.createElement('div');
+        div.classList.add('newBook');
+        const newTitle = document.createElement('div');
+        newTitle.classList.add('title');
+        newTitle.textContent = title.value;
+
+        const newAuthor = document.createElement('div');
+        newAuthor.classList.add('author');
+        newAuthor.textContent = author.value;
+
+        const newPages = document.createElement('div');
+        newPages.classList.add('pages');
+        newPages.textContent = pages.value+' pages';
+    
+        newBook.appendChild(div);
+        div.appendChild(newTitle);
+        div.appendChild(newAuthor);
+        div.appendChild(newPages);
+
+        if(read.checked){
+            const newRead = document.createElement('div');
+            newRead.classList.add('read');
+            newRead.textContent = read.value;
+            newPages.appendChild(newRead);
+        }
+
+        const remove = document.createElement('button');
+        remove.classList.add('remove');
+        remove.textContent = 'Remove';
+        div.appendChild(remove);
+        const removeNew = document.querySelector('.remove');
+        removeNew.addEventListener('click', () => {
+            removeBook();
+            newBook.removeChild(div);
+        })
+
+
+        console.log("Form has been submitted");
 }
 
 function displayArray(){
@@ -41,3 +87,16 @@ displayArray();
 function displayForm(){
    form.classList.toggle("active");
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    console.log(myLibrary);
+});
+
+function removeBook(){
+    myLibrary.pop();
+    console.log(myLibrary);
+}
+
+
